a12657e81b1464488d6c86b5b1b9dca1a345126b
nicwineburger <nweinber@umich.edu>
2022-01-23 14:54:32 -0600

    Add disconnet

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./auth.json');
 
-// Get general channel by ID
+// Needed id's for running
 const generalID = '365275098229506049';
 const coolZoneID = '715920210892161046';
 const mordorRoleID = '822523543581229086';
@@ -21,7 +21,7 @@ const client = new Client({
 client.once('ready', () => {
     client.user.setPresence({
         activities: [{ 
-          name: "For TheCoolZone parties",
+          name: "TheCoolZone",
           type: "WATCHING"
         }],
         status: "online"
@@ -29,21 +29,33 @@ client.once('ready', () => {
 	console.log('PartyBot is online!');
 });
 
-// Login to Discord with your client's token
-client.login(token);
+// Change status when bot is disconnected
+client.on("disconnect", () => {
+    client.user.setPresence({
+        activities: [{ 
+          name: "dead",
+          type: "PLAYING"
+        }],
+        status: "idle"
+    })
+	console.log('PartyBot is offline!'); 
+})
 
-
+// Send message to all users with role when voice channel has more than 4 people in it
 client.on("voiceStateUpdate", (oldState, newState) => {
     const genChannel = client.channels.cache.get(generalID);
     const coolZone = client.channels.cache.get(coolZoneID); 
     console.log("voice state changed");
-  
+    
     let mordorRole = genChannel.guild.roles.cache.get(mordorRoleID);
     if (oldState.channelId !== coolZoneID && newState.channelId === coolZoneID) {
         let memberCount = coolZone.members.size;
         if (memberCount > 4) {
             genChannel.send(mordorRole.toString() + ": there are " + memberCount + " people in The Cool Zone!\nLet's party!");
         }
-
+        
     }
 })
+
+// Login to Discord with your client's token
+client.login(token);
